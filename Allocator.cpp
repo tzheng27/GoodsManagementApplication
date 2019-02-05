e2d02bc62180674441edcec8569b476a246ce697
Tian Zheng <zhengtian07@gmail.com>
2019-02-05 12:00:05 -0500

    Update and rename ms5_Allocator.cpp to Allocator.cpp

---
+++ Allocator.cpp
@@ -0,0 +1,18 @@
+
+#include "Good.h"
+#include "Perishable.h"
+
+namespace aid {
+
+	// creates an empty good
+	//
+	iGood* CreateGood() {
+		return new Good();
+	}
+
+	// creates an empty perishable good
+	//
+	iGood* CreatePerishable() {
+		return new Perishable();
+	}
+}
