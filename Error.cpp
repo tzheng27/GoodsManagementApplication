7752a1567bb7695bad80c039f52d1c4737cd9cdc
Tian Zheng <zhengtian07@gmail.com>
2019-02-05 12:01:23 -0500

    Update Error.cpp

--- Error.cpp
+++ Error.cpp
@@ -1,14 +1,4 @@
-// Final Project Milestone 2
-//
-// Version 1.0
-// Date   Oct 30, 2018
-// Author Tian Zheng
-// Description
-//
-// Revision History
-// -----------------------------------------------------------
-// Name               Date                 Reason
-/////////////////////////////////////////////////////////////////
+
 
 #include <iostream>
 #include <cstring>
